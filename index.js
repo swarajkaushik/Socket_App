bafedd59c2864e1dcc39fba7bdafc718f39092e5
Swaraj Kaushik <swarajkaushik@gmail.com>
2024-02-26 20:56:03 +0530

    added code

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const io = socketio(server);
 io.on("connection", (socket) => {
   console.log("a new user connected", socket.id);
 
-  socket.on("from_client", () => {
-    console.log("event coming from the client");
+  socket.on("msg_send", (data) => {
+    console.log(data);
+    // io.emit("msg_rcvd", data);
+    // socket.emit("msg_rcvd", data); -> user which send this msg will only receive
+    socket.broadcast.emit("msg_rcvd", data); // -> except the user who send will receive the msg
   });
-
-  setInterval(() => {
-    socket.emit("from_server");
-  }, 2000);
 });
 
 app.use("/", express.static(__dirname + "/public"));
